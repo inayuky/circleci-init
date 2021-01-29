686fd214cbc74f89f014ead038cc1ad07e13be92
inayuky <inayuky@gmail.com>
2021-01-29 18:20:12 +0900

    add double quote to variables for irregular path

--- create_prj.sh
+++ create_prj.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
 
 # check repogitory name is given
 if [ -z "$1" ]; then
@@ -8,18 +8,18 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-REPO_NAME=$1
+REPO_NAME="$1"
 
 # define destination directory
 if [ -n "$2" ]; then
   # second argument
-  DEST_DIR=$2
+  DEST_DIR="$2"
 else
   # export dotenv for destination directory
-  DOTENV_FILE="${SCRIPT_DIR}/.env"
+  DOTENV_FILE=""${SCRIPT_DIR}"/.env"
   if [ -e "$DOTENV_FILE" ]; then
     set -a
-    source $DOTENV_FILE
+    source "$DOTENV_FILE"
     set +a
   fi
 fi
@@ -43,12 +43,12 @@ echo "dest_dir = ${DEST_DIR}"
 echo "repo_name = ${REPO_NAME}"
 
 # create repogitory at github from template repogitory
-cd $DEST_DIR
-gh repo create -y $REPO_NAME --private -p circleci-template
+cd "$DEST_DIR"
+gh repo create -y "$REPO_NAME" --private -p circleci-template
 
 sleep 1 # wait for creating github repogitory
 
-cd $REPO_NAME
+cd "$REPO_NAME"
 git pull origin main
 git branch -M main
 git branch -u origin/main
